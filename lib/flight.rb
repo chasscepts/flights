ee755c84324b8b926f06156f42955b9260a0bf3e
chasscepts <francischassobetta@gmail.com>
2021-04-01 15:39:55 +0100

    add FlightDeal class

---
+++ lib/flight.rb
@@ -0,0 +1,11 @@
+class FlightDeal
+  attr_reader :from, :to, :date, :time, :price
+
+  def initialize(from, to, price, date, time)
+    @from = from
+    @to = to
+    @price = price
+    @date = date
+    @time = time
+  end
+end
